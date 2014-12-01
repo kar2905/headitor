ed0e2a055dbb174d5a765e9cd3e3a240497c4fa3
chrisorwa <chrisorwa@gmail.com>
2014-12-01 07:12:24 +0300

    Create mapper.py

---
+++ selection/mapper.py
@@ -0,0 +1,14 @@
+
+'''
+Map Social Media Platforms
+
+'''
+
+# map platform to emotional value
+platform = dict(
+LinkedIn = ['False','positve'] ,
+Facebook = ['True','negative'],
+Twitter = ['True','negative'],
+)
+
+
